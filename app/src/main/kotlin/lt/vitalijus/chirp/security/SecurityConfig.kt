125d5f9af0692d429df26357285b52e1a3c2c91d
Vitalikas <kolinkv@gmail.com>
2025-10-15 14:40:32 +0300

    add jwt auth filter to security configuration for enhanced token validation and access control

--- app/src/main/kotlin/lt/vitalijus/chirp/security/SecurityConfig.kt
+++ app/src/main/kotlin/lt/vitalijus/chirp/security/SecurityConfig.kt
@@ -1,6 +1,7 @@
 package lt.vitalijus.chirp.security
 
 import jakarta.servlet.DispatcherType
+import lt.vitalijus.chirp.api.config.JwtAuthFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
@@ -8,12 +9,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 class SecurityConfig {
 
     @Bean
-    fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+    fun filterChain(
+        httpSecurity: HttpSecurity,
+        jwtAuthFilter: JwtAuthFilter
+    ): SecurityFilterChain {
         return httpSecurity
             .csrf { it.disable() }
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
@@ -21,6 +26,8 @@ class SecurityConfig {
                 auth
                     .requestMatchers("/api/auth/**")
                     .permitAll()
+                    .requestMatchers("/api/auth/change-password")
+                    .authenticated()
                     .dispatcherTypeMatchers(
                         DispatcherType.ERROR,
                         DispatcherType.FORWARD
@@ -29,6 +36,7 @@ class SecurityConfig {
                     .anyRequest()
                     .authenticated()
             }
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
             .exceptionHandling { configurer ->
                 configurer
                     .authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
