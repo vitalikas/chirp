7d882b747576321a1eb075c31df989299f00b4a8
Vitalikas <kolinkv@gmail.com>
2025-10-27 10:59:30 +0200

    add chat participant entity for database representation of chat users

---
+++ chat/src/main/kotlin/lt/vitalijus/chirp/chat/infra/database/entities/ChatParticipantEntity.kt
@@ -0,0 +1,32 @@
+package lt.vitalijus.chirp.chat.infra.database.entities
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Index
+import jakarta.persistence.Table
+import lt.vitalijus.chirp.domain.events.type.UserId
+import org.hibernate.annotations.CreationTimestamp
+import java.time.Instant
+
+@Entity
+@Table(
+    name = "chat_participants",
+    schema = "chat_service",
+    indexes = [
+        Index(name = "idx_chat_participants_username", columnList = "username"),
+        Index(name = "idx_chat_participants_email", columnList = "email")
+    ]
+)
+class ChatParticipantEntity(
+    @Id
+    var userId: UserId,
+    @Column(nullable = false, unique = true)
+    var username: String,
+    @Column(nullable = false, unique = true)
+    var email: String,
+    @Column(nullable = true)
+    var profilePictureUrl: String? = null,
+    @CreationTimestamp
+    var createdAt: Instant = Instant.now()
+)
