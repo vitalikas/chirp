78f60c9c5a94d2c16736f16f24d18cc06e5e6152
Vitalikas <kolinkv@gmail.com>
2025-11-12 11:52:39 +0200

    add dependency management for spring boot dependencies to ensure consistent versions

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,8 +1,18 @@
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+
 plugins {
     id("chirp.kotlin-common")
+
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 dependencies {
-
     implementation(libs.spring.boot.starter.security)
+}
+
+
+configure<DependencyManagementExtension> {
+    imports {
+        mavenBom("org.springframework.boot:spring-boot-dependencies:${libraries.findVersion("spring-boot").get()}")
+    }
 }
