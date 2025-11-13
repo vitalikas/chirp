386326f93a3effe388ea7cca4dfccfa77c1d3d52
Vitalikas <kolinkv@gmail.com>
2025-11-13 14:42:54 +0200

    update create chat request validation to allow at least one participant

--- chat/src/main/kotlin/lt/vitalijus/chirp/api/dto/CreateChatRequest.kt
+++ chat/src/main/kotlin/lt/vitalijus/chirp/api/dto/CreateChatRequest.kt
@@ -5,7 +5,7 @@ import lt.vitalijus.chirp.domain.events.type.UserId
 
 data class CreateChatRequest(
     @field:Size(
-        min = 2,
+        min = 1,
         message = "Chats must have at least 2 unique participants"
     )
     val otherUserIds: List<UserId>
