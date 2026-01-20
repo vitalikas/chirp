1fa920a3663027628041097cdae0d2fae11501d0
Vitalikas <kolinkv@gmail.com>
2026-01-20 11:54:21 +0200

    configure build to include resource files from notification and user modules in the jar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,18 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
 	id("chirp.spring-boot-app")
+}
+
+tasks {
+	named<BootJar>("bootJar") {
+		from(project(":notification").projectDir.resolve("src/main/resources")) {
+			into("")
+		}
+		from(project(":user").projectDir.resolve("src/main/resources")) {
+			into("")
+		}
+	}
 }
 
 dependencies {
